3c46f586042ec72b64e57e8e0349f06e4fb64e23
jimmybnguyen <me@jimmybnguyen.com>
2016-09-17 10:36:22 -0700

    add notes and scripts

---
+++ Assets/Scripts/NoteResizing.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteResizing : MonoBehaviour
+{
+    public float speed;
+    public 
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += Vector3.one * speed * Time.deltaTime;
+    }
+}

66dcb548d9d6d898f20b62fe473d76ee997b8e40
jimmybnguyen <me@jimmybnguyen.com>
2016-09-17 08:34:25 -0700

    add contactNotes script to scripts folder

---
+++ Assets/Scripts/ContactNotes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+
+public class ContactNotes : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+
+	void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void Update () {
+	
+	}
+}

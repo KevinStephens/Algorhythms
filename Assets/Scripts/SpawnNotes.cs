ca49c8b73b6ac4595e40bce4b3b5c93b8e3fc4a6
Phansa Chaonpoj <Phansa@D-140-142-161-92.dhcp4.washington.edu>
2016-09-17 17:51:18 -0700

    spawn script

---
+++ Assets/Scripts/SpawnNotes.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+public class SpawnNotes : MonoBehaviour {
+	public GameObject prefab;
+
+	private struct position
+	{
+		public float x;
+		public float y;
+		public float z;
+	}
+
+	private struct note
+	{
+		public position pos;
+		public long tick;
+		public string hand;
+		public bool spawned;
+	
+	}
+	private long offsetticks;
+	List<note> MySongNotes;
+	private long startingticks;
+
+	// Use this for initialization
+	void Awake () {
+
+    string filePath = System.IO.Path.GetFullPath("TwinkleV1.txt");
+
+
+		StreamReader sr = new StreamReader (filePath);
+		startingticks = System.DateTime.Now.Ticks;
+		List<note> MySongNotes = new List<note>();
+		while (!sr.EndOfStream)
+		{
+			string Note = sr.ReadLine();
+			long tempticks;
+			note TempNote = new note();
+
+			string[] tempinput = Note.Split(',');
+			float.TryParse(tempinput[0], out TempNote.pos.x);
+			float.TryParse(tempinput[1], out TempNote.pos.y);
+			float.TryParse(tempinput[2], out TempNote.pos.z);
+			long.TryParse(tempinput[3], out tempticks);
+			TempNote.tick = tempticks - startingticks;
+			MySongNotes.Add(TempNote);
+		}
+	
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("WHY IS IT NOT WORKING");
+		long currentticks = System.DateTime.Now.Ticks;
+		offsetticks = currentticks - startingticks;
+
+		foreach(var n in MySongNotes) {
+			if (n.tick < offsetticks && n.spawned == false) {
+				//spawn note
+				Instantiate(prefab, new Vector3(n.pos.x, n.pos.y, n.pos.z), Quaternion.identity);
+				//n.spawned = true;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TimingHolder.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimingHolder {
+
+	private float x, y, z;
+	private long tick;
+	private bool value;
+
+	public TimingHolder(float x, float y, float z, long tick, bool value)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.tick = tick;
+		this.value = value;
+	}
+		
+
+	public float X {
+		get;
+		set;
+	}
+	public float Y
+	{
+		get;
+		set; 
+	}
+	public float Z
+	{
+		get;
+		set;
+	}
+	public long Tick
+	{
+		get;
+		set;
+	}
+	public bool Value
+	{
+		get;
+		set;
+	}
+
+
+}

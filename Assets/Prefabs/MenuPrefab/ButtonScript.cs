72898c82d017796e4219295a1140067834c7fe3e
Kevin Stephens <kevin.stephens@oregonstate.edu>
2016-09-18 11:34:56 -0700

    Added menu prefab, new menu scene

---
+++ Assets/Prefabs/MenuPrefab/ButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using VRTK;
+
+public class ButtonTest : MonoBehaviour
+{
+    public GameObject button;
+    public GameObject controller;
+
+    private void Start(){
+        //Setup controller event listeners
+        controller.GetComponent<VRTK_UIPointer>().UIPointerElementExit += ButtonOnExit;
+    }
+
+    private void ButtonOnExit(object sender, UIPointerEventArgs e)
+    {
+        Debug.Log("UI Pointer exited " + e.previousTarget.name + " on Controller index [" + e.controllerIndex + "]");
+        Debug.Log("YEEAAAH");
+
+
+        if (e.previousTarget == button)
+        {
+            Debug.Log("YEEAAAH!!!!!!!!!!!");
+        }
+    }
+}

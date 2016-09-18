3de004445c1925d4512698f90744feecb13a708d
jimmybnguyen <me@jimmybnguyen.com>
2016-09-17 22:11:53 -0700

    add ontriggerstay, and the stage

--- Assets/Scripts/ContactNotes.cs
+++ Assets/Scripts/ContactNotes.cs
@@ -9,7 +9,10 @@ public class ContactNotes : MonoBehaviour
 
     SteamVR_TrackedObject trackedObj;
     SteamVR_Controller.Device device;
-
+    public int perfect;
+    public int good;
+    public int okay;
+    public int bad;
 
     void Awake()
     {
@@ -63,6 +66,46 @@ public class ContactNotes : MonoBehaviour
 
         collidedNote.destroy();
     }
+    void OnTriggerStay(Collider col)
+    {
+        Debug.Log("you have stay collided with " + col.gameObject.name);
 
+        //this is device is the wand. 
+
+
+        NoteScript collidedNote = col.GetComponent<NoteScript>();
+        //if we touch the trigger with the wand.....
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //small debug
+            Debug.Log("The controller is being held down and touching something....");
+            Transform point = collidedNote.getScale();
+            float percent = collidedNote.getPercent(point);
+
+            if (percent >= 90)
+            {
+                //perfect
+                perfect++;
+                Debug.Log("perfect: " + perfect);
+            }
+            else if (percent >= 80 && percent < 90)
+            {
+                //gg
+                good++;
+                Debug.Log("good: " + good);
+            }
+            else if (percent >= 60 && percent < 80)
+            {
+                okay++;
+                Debug.Log("okay: " + okay);
+            }
+            else
+            {
+                bad++;
+                Debug.Log("bad: " + bad);
+            }
+            collidedNote.destroy();
+        }
+    }
 
 }

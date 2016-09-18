2b37c33dfb5a69eac71d6d453760020d2fe1ee66
Kevin Stephens <kevin.stephens@oregonstate.edu>
2016-09-18 12:19:09 -0700

    Added UI stuff!

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using VRTK;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject button;
+    public GameObject controller;
+
+    private void Start(){
+        //Setup controller event listeners
+        controller.GetComponent<VRTK_UIPointer>().UIPointerElementEnter += ButtonOnEnter;
+        controller.GetComponent<VRTK_UIPointer>().UIPointerElementExit += ButtonOnExit;
+    }
+
+    private void ButtonOnExit(object sender, UIPointerEventArgs e)
+    {
+        Debug.Log("UI Pointer exited " + e.previousTarget.name + " on Controller index [" + e.controllerIndex + "]");
+        Debug.Log("YEEAAAH");
+
+
+        if (e.previousTarget == button)
+        {
+            Debug.Log("YEEAAAH!!!!!!!!!!!");
+        }
+    }
+
+    private void ButtonOnEnter(object sender, UIPointerEventArgs e)
+    {
+        Debug.Log("UI Pointer entered " + e.currentTarget.name + " on Controller index [" + e.controllerIndex + "]");
+
+        if (e.currentTarget.GetType() == typeof(Button))
+        {
+            Button target = e.currentTarget.GetComponent<Button>();
+            target.Select();
+        }
+    }
+}
